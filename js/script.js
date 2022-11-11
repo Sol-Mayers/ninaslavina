bf48965aaec9ee51253cdfaa57bc37cd1ea4a7cc
Sol-Mayers <rinotello@yandex.ru>
2022-11-11 17:08:13 +0300

    added slider

--- js/script.js
+++ js/script.js
@@ -691,4 +691,20 @@ $(".scrollto a").on("click", function () {
   return false;
 });
 
-//PDF js
+//slider
+let swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
